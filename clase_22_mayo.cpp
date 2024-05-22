6ad4966ad62bf8aefede99e903cb48f9ddf42594
Daniross27 <163915781+Daniross27@users.noreply.github.com>
2024-05-22 12:43:10 -0600

    Add files via upload

---
+++ clase_22_mayo.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+ string nombre[3]={"5","hola","2""adios"};
+ int vector2[]={1,2,3,4,10,9,80};
+ //
+ int vector2[3];
+ vector2[0]=1;
+ vector2[1]=3;
+ vector2[2]=10;
+
+
+    return 0;
+}
